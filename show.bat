cffb757f9567cc628c14346edc4b3ed1e641a421
ItsChanGat <youness-changat@hotmail.com>
2024-10-27 17:18:31 +0100

    Update show.bat

--- show.bat
+++ show.bat
@@ -4,12 +4,6 @@ python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
 python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
 python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
 python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
-
-echo   ____ _                  ____       _   
-echo  / ___| |__   __ _ _ __  / ___| __ _| |_ 
-echo | |   | '_ \ / _` | '_ \| |  _ / _` | __|
-echo | |___| | | | (_| | | | | |_| | (_| | |_ 
-echo  \____|_| |_|\__,_|_| |_|\____|\__,_|\__|
                                           
 echo User name : runneradmin
 echo User Pass : Changat
