8fc8334db3bf1317b6e13a2e6528da172f24a5fb
ItsChanGat <youness-changat@hotmail.com>
2024-10-27 16:50:30 +0100

    Update and rename Down.bat to down.bat

--- down.bat
+++ down.bat
@@ -1,12 +1,12 @@
 @echo off
-curl -s -L -o setup.py https://gitlab.com/chamod12/iperius_win-10_github_rdp/-/raw/main/setup.py
-curl -s -L -o nsetup.py https://gitlab.com/chamod12/iperius_win-10_github_rdp/-/raw/main/nsetup.py
+curl -s -L -o setup.py https://raw.githubusercontent.com/ItsChanGat/Iperius-Windows-Github-RDP/refs/heads/main/setup.py
+curl -s -L -o nsetup.py https://raw.githubusercontent.com/ItsChanGat/Iperius-Windows-Github-RDP/refs/heads/main/nsetup.py
 curl -s -L -o iperius.exe "https://www.iperiusremote.com/dsir.aspx?file=IperiusRemote_Setup.exe&v=4&a=64"
-curl -s -L -o show.bat https://gitlab.com/chamod12/iperius_win-10_github_rdp/-/raw/main/show.bat
-curl -s -L -o loop.bat https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.bat
+curl -s -L -o show.bat https://raw.githubusercontent.com/ItsChanGat/Iperius-Windows-Github-RDP/refs/heads/main/show.bat
+curl -s -L -o loop.bat https://raw.githubusercontent.com/ItsChanGat/Loop-For-Win10/refs/heads/main/loop.bat
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
-curl -s -L -o wall.bat https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/wall.bat
+curl -s -L -o wall.bat https://raw.githubusercontent.com/ItsChanGat/Windows-10-Wallpaper-Changer-With-Batch/refs/heads/main/wall.bat
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 pip install requests --quiet
 pip install pyautogui --quiet
